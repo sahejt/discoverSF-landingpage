253cda325b26c670d43483cb7907390766f312b5
sahejt <sahejtuli071101@gmail.com>
2023-05-01 20:37:04 -0700

    Added menu hover effect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,16 +8,56 @@ const Navbar = () => {
       <h3 className="flex flex-col font-black text-4xl leading-[85%]">
         <span>Discover</span> <span>San Francisco</span>
       </h3>
-     
+      <div className="flex gap-6">
+        <div className="menu-item">Home</div>
+        <div className="menu-item">Things to Do</div>
+        <div className="menu-item">Neighborhoods</div>
+        <div className="menu-item">Food & Drink</div>
+        <div className="menu-item">Events</div>
+        <div className="menu-item">Insider Tips</div>
+      </div>
 
-      <motion.div variants={wordAnimation} className="flex gap-8">
-        <div>Home</div>
-        <div>Things to Do</div>
-        <div>Neighborhoods</div>
-        <div>Food & Drink</div>
-        <div>Events</div>
-
-      </motion.div>
+      <style jsx>{`
+        .menu-item {
+          position: relative;
+        }
+        .menu-item::before {
+          content: "";
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: -2px;
+          height: 2px;
+          background-color: #000;
+          transform: scaleX(0);
+          transform-origin: 0% 50%;
+          transition: transform 0.7s;
+        }
+        .menu-item:hover::before {
+          transform: scaleX(1);
+        }
+        .menu-item:hover::after {
+          content: "";
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: -2px;
+          height: 2px;
+          background-color: #000;
+          animation: underline 0.7s forwards;
+        }
+        @keyframes underline {
+          0% {
+            transform: scaleX(0);
+          }
+          50% {
+            transform: scaleX(1);
+          }
+          100% {
+            transform: scaleX(0);
+          }
+        }
+      `}</style>
     </nav>
   );
 };
